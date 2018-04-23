f10e1234e82132346031759e7bac7efec3d55730
malin4 <2rmalinowski@gmail.com>
2018-04-23 20:32:55 +0200

    comment model created

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path(r'', views.post_list, name='post_list'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path(r'comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path(r'comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
+from django.views.generic import ListView, View
 
+
+# class PostView(ListView):
+#     queryset = Post.objects.all()
+#     context_object_name = 'post_list'
+#     template_name = 'blog_app/post_list.html'
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog_app/post_list.html', {'posts': posts})
 
 
@@ -26,3 +32,31 @@ def post_new(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog_app/post_detail.html', {'post': post})
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog_app/add_comment_to_post.html', {'form': form})
+
+
+# @login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+# @login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
